b74e490151dea57819256471174a46f235793f74
Crawron <azertyaaron@gmail.com>
2018-05-10 23:09:51 -0500

    Helper not needed anymore

--- src/Scene.ts
+++ src/Scene.ts
@@ -2,8 +2,6 @@ import { Scene, AxesHelper } from "three"
 import { hemisphereLight, directionalLight } from "./Lighting"
 
 
-const helper = new AxesHelper(10)
 export const mainScene = new Scene
 
-mainScene.add(helper)
 mainScene.add(hemisphereLight, directionalLight)
