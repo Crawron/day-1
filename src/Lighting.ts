265c01d31b1239092d328fad76b4aae6a50ded11
Crawron <azertyaaron@gmail.com>
2018-05-11 12:54:01 -0500

    Increased the intensity of the hemisphere light

--- src/Lighting.ts
+++ src/Lighting.ts
@@ -1,7 +1,7 @@
 import { HemisphereLight, DirectionalLight } from 'three'
 
 
-export const hemisphereLight = new HemisphereLight(0xffffff, 0x101010, .8)
+export const hemisphereLight = new HemisphereLight(0xffffff, 0x101010, 1)
 
 export const directionalLight = new DirectionalLight(0xffffff)
 directionalLight.position.set(0, 10, 2)
