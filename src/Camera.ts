afb4bff80c74fff033a6943be302b92f456c31ec
Crawron <azertyaaron@gmail.com>
2018-05-10 23:08:33 -0500

    Set up camera position

--- src/Camera.ts
+++ src/Camera.ts
@@ -1,4 +1,10 @@
-import { OrthographicCamera, PerspectiveCamera } from "three"
+import { OrthographicCamera, Vector3 } from "three"
 
-export const mainCamera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight)
-mainCamera.position.z = 5
+const zoom = .03
+const xRadius = window.innerWidth / 2 * zoom
+const yRadius = window.innerHeight / 2 * zoom
+
+export const mainCamera = new OrthographicCamera(-xRadius, xRadius, yRadius, -yRadius)
+
+mainCamera.position.set(15, 15, 15)
+mainCamera.lookAt(new Vector3(0, 0, 0))
