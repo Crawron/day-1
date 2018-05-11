a961d97a2e31740335d9260489bca2ca4cfeecf0
Crawron <azertyaaron@gmail.com>
2018-05-11 14:27:26 -0500

    Made Init.ts the new main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,44 @@
-import { init } from "./Init"
+import { JSONLoader, MeshLambertMaterial, Mesh, LoadingManager } from "three"
+import { Fez } from "./Fez"
+import { animate } from "./Animate"
+import { mainScene } from "./Scene"
+import { hemisphereLight } from "./Lighting"
+
+
+export let fez: Fez
+
+const loadingManager = new LoadingManager
+loadingManager.onProgress = () => {
+	loadScene()
+	animate()
+}
+
+export function init() {
+	loadFezMesh()
+}
+
+function loadFezMesh() {
+	// Materials
+	const redMaterial = new MeshLambertMaterial({ color: 0xdd1111 })
+	const redShadedMaterial = new MeshLambertMaterial({ color: 0xcc0505 })
+	const yellowMaterial = new MeshLambertMaterial({ color: "yellow" })
+
+	const materials = [redMaterial, yellowMaterial, redShadedMaterial]
+
+	// Mesh
+	const loader = new JSONLoader(loadingManager)
+	loader.load('./assets/models/fez.json', geometry => {
+		const mesh = new Mesh(geometry, materials)
+		fez = new Fez(mesh)
+	})
+}
+
+function loadScene() {
+	// Lighting
+	mainScene.add(hemisphereLight)
+
+	// Meshes
+	mainScene.add(fez.mesh)
+}
 
 init()
